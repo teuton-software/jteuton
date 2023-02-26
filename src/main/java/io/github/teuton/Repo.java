05dfc5772fa28ff88907ab101be8c0d1641f4803
fvarrui <4354486+fvarrui@users.noreply.github.com>
2023-02-26 11:17:42 +0000

    A object model

---
+++ src/main/java/io/github/teuton/Repo.java
@@ -0,0 +1,80 @@
+package io.github.teuton;
+
+import java.net.URL;
+import java.util.Map;
+
+import io.github.teuton.utils.URLUtils;
+
+public class Repo {
+
+	private String name;
+	private String description;
+	private String type;
+	private URL url;
+	private boolean enable = true;
+	private Map<String, String> metadata;
+	
+	public Repo(String name, Map<String, String> metadata) {
+		setName(name);
+		setDescription(metadata.remove("description"));
+		setType(metadata.remove("type"));
+		setUrl(URLUtils.newUrl(metadata.remove("URL")));
+		setEnable(Boolean.parseBoolean(metadata.remove("enable")));
+		setMetadata(metadata);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public URL getUrl() {
+		return url;
+	}
+
+	public void setUrl(URL url) {
+		this.url = url;
+	}
+
+	public boolean isEnable() {
+		return enable;
+	}
+
+	public void setEnable(boolean enable) {
+		this.enable = enable;
+	}
+
+	public Map<String, String> getMetadata() {
+		return metadata;
+	}
+
+	public void setMetadata(Map<String, String> metadata) {
+		this.metadata = metadata;
+	}
+
+	@Override
+	public String toString() {
+		return "Repo [name=" + name + ", description=" + description + ", type=" + type + ", url=" + url + ", enable="
+				+ enable + ", metadata=" + metadata + "]";
+	}
+
+}
---
+++ src/main/java/io/github/teuton/Test.java
@@ -0,0 +1,150 @@
+package io.github.teuton;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Test {
+
+	private String id;
+	private String repo;
+	private String name;
+	private List<String> authors = new ArrayList<>();
+	private LocalDate date;
+	private String description;
+	private List<String> tags = new ArrayList<>();
+	private List<File> files = new ArrayList<>();
+	private File testDir;
+	private File configFile;
+	private File startFile;
+	private Map<String, String> metadata = new HashMap<>();
+
+	public Test(String id, String repo) {
+		this.id = id;
+		this.repo = repo;
+	}
+
+	public Test(File testFolder) throws FileNotFoundException {
+		if (!testFolder.exists() || !testFolder.isDirectory()) {
+			throw new FileNotFoundException("test " + testFolder.getName() + "not found!");
+		}
+		this.testDir = testFolder;
+	}
+
+	public Test() {
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getRepo() {
+		return repo;
+	}
+
+	public void setRepo(String repo) {
+		this.repo = repo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<String> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(List<String> authors) {
+		this.authors = authors;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<String> getTags() {
+		return tags;
+	}
+
+	public void setTags(List<String> tags) {
+		this.tags = tags;
+	}
+
+	public List<File> getFiles() {
+		return files;
+	}
+
+	public void setFiles(List<File> files) {
+		this.files = files;
+	}
+
+	public File getTestDir() {
+		return testDir;
+	}
+
+	public void setTestDir(File testDir) {
+		this.testDir = testDir;
+	}
+
+	public void setConfigFile(File configFile) {
+		this.configFile = configFile;
+	}
+
+	public void setStartFile(File startFile) {
+		this.startFile = startFile;
+	}
+
+	public File getConfigFile() {
+		if (configFile == null) {
+			configFile = new File(testDir, "config.yaml");
+		}
+		return configFile;
+	}
+
+	public File getStartFile() {
+		if (startFile == null) {
+			startFile = new File(testDir, "start.rb");
+		}
+		return startFile;
+	}
+
+	public Map<String, String> getMetadata() {
+		return metadata;
+	}
+
+	public void setMetadata(Map<String, String> metadata) {
+		this.metadata = metadata;
+	}
+
+	@Override
+	public String toString() {
+		return "Test [id=" + id + ", repo=" + repo + ", name=" + name + ", authors=" + authors + ", date=" + date
+				+ ", description=" + description + ", tags=" + tags + ", files=" + files + ", testDir=" + testDir
+				+ ", configFile=" + configFile + ", startFile=" + startFile + ", metadata=" + metadata + "]";
+	}
+
+}
